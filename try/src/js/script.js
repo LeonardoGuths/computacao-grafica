484994791d07d343d09263c50da44a0112d28894
LeonardoGuths <ldsguths@inf.ufpel.edu.br>
2022-09-27 15:32:16 -0300

    updating files

--- try/src/js/script.js
+++ try/src/js/script.js
@@ -1,49 +1,337 @@
-function main() {
-  const { gl, meshProgramInfo } = initializeWorld();
+"use strict";
 
-  const cubeTranslation = [0, 0, 0];
+var vs = `#version 300 es
 
-  const cubeBufferInfo = flattenedPrimitives.createCubeBufferInfo(gl, 20);
+in vec4 a_position;
+in vec4 a_color;
 
-  const cubeVAO = twgl.createVAOFromBufferInfo(
-    gl,
-    meshProgramInfo,
-    cubeBufferInfo
-  );
+uniform mat4 u_matrix;
 
-  var fieldOfViewRadians = degToRad(90);
+out vec4 v_color;
 
-  const cubeUniforms = {
-    u_colorMult: [1, 0.5, 0.5, 1],
-    u_matrix: m4.identity(),
-  };
+void main() {
+  // Multiply the position by the matrix.
+  gl_Position = u_matrix * a_position;
 
-  function computeMatrix(viewProjectionMatrix, translation, yRotation) {
-    var matrix = m4.translate(
-      viewProjectionMatrix,
-      translation[0],
-      translation[1],
-      translation[2]
-    );
+  // Pass the color to the fragment shader.
+  v_color = a_color;
+}
+`;
 
-    return m4.yRotate(matrix, yRotation);
+var fs = `#version 300 es
+precision highp float;
+
+// Passed in from the vertex shader.
+in vec4 v_color;
+
+uniform vec4 u_colorMult;
+uniform vec4 u_colorOffset;
+
+out vec4 outColor;
+
+void main() {
+   outColor = v_color * u_colorMult + u_colorOffset;
+}
+`;
+
+const degToRad = (d) => (d * Math.PI) / 180;
+
+const radToDeg = (r) => (r * 180) / Math.PI;
+
+var config = {
+  rotate: degToRad(20),
+  x: 0,
+  y: 0,
+  addCaixa: function () {
+    console.log(cubeBufferInfo);
+    console.log(cubeVAO);
+  },
+};
+
+const loadGUI = () => {
+  const gui = new dat.GUI();
+  gui.add(config, "rotate", 0, 20, 0.5);
+  gui.add(config, "x", -150, 150, 5);
+  gui.add(config, "y", -100, 100, 5);
+  gui.add(config, "addCaixa");
+};
+
+var TRS = function () {
+  this.translation = [0, 0, 0];
+  this.rotation = [0, 0, 0];
+  this.scale = [1, 1, 1];
+};
+
+TRS.prototype.getMatrix = function (dst) {
+  dst = dst || new Float32Array(16);
+  var t = this.translation;
+  var r = this.rotation;
+  var s = this.scale;
+
+  // compute a matrix from translation, rotation, and scale
+  m4.translation(t[0], t[1], t[2], dst);
+  m4.xRotate(dst, r[0], dst);
+  m4.yRotate(dst, r[1], dst);
+  m4.zRotate(dst, r[2], dst);
+  m4.scale(dst, s[0], s[1], s[2], dst);
+  return dst;
+};
+
+var Node = function (source) {
+  this.children = [];
+  this.localMatrix = m4.identity();
+  this.worldMatrix = m4.identity();
+  this.source = source;
+};
+
+Node.prototype.setParent = function (parent) {
+  // remove us from our parent
+  if (this.parent) {
+    var ndx = this.parent.children.indexOf(this);
+    if (ndx >= 0) {
+      this.parent.children.splice(ndx, 1);
+    }
   }
 
-  loadGUI();
+  // Add us to our new parent
+  if (parent) {
+    parent.children.push(this);
+  }
+  this.parent = parent;
+};
 
-  function render() {
+Node.prototype.updateWorldMatrix = function (matrix) {
+  var source = this.source;
+  if (source) {
+    source.getMatrix(this.localMatrix);
+  }
+
+  if (matrix) {
+    // a matrix was passed in so do the math
+    m4.multiply(matrix, this.localMatrix, this.worldMatrix);
+  } else {
+    // no matrix was passed in so just copy.
+    m4.copy(this.localMatrix, this.worldMatrix);
+  }
+
+  // now process all the children
+  var worldMatrix = this.worldMatrix;
+  this.children.forEach(function (child) {
+    child.updateWorldMatrix(worldMatrix);
+  });
+};
+
+var cubeVAO;
+var cubeBufferInfo;
+
+function main() {
+  // Get A WebGL context
+  /** @type {HTMLCanvasElement} */
+  var canvas = document.querySelector("#canvas");
+  var gl = canvas.getContext("webgl2");
+  if (!gl) {
+    return;
+  }
+
+  loadGUI(gl);
+
+  // Tell the twgl to match position with a_position, n
+  // normal with a_normal etc..
+  twgl.setAttributePrefix("a_");
+
+  cubeBufferInfo = flattenedPrimitives.createCubeBufferInfo(gl, 1);
+
+  // setup GLSL program
+  var programInfo = twgl.createProgramInfo(gl, [vs, fs]);
+
+  cubeVAO = twgl.createVAOFromBufferInfo(gl, programInfo, cubeBufferInfo);
+
+  function degToRad(d) {
+    return (d * Math.PI) / 180;
+  }
+
+  var fieldOfViewRadians = degToRad(60);
+
+  var objectsToDraw = [];
+  var objects = [];
+  var nodeInfosByName = {};
+
+  // Let's make all the nodes
+  var blockGuyNodeDescriptions = {
+    name: "point between feet",
+    draw: false,
+    children: [
+      {
+        name: "waist",
+        translation: [0, 3, 0],
+        children: [
+          {
+            name: "torso",
+            translation: [0, 1, 0],
+            children: [
+              {
+                name: "neck",
+                translation: [0, 0, 1],
+                children: [
+                  {
+                    name: "head",
+                    translation: [0, 0, 1],
+                    children: [
+                      {
+                        name: "hat1",
+                        translation: [0, 1, 0],
+                      },
+                      {
+                        name: "hat2",
+                        translation: [0, 1, 0],
+                      },
+                    ],
+                  },
+                ],
+              },
+              {
+                name: "left-arm",
+                translation: [-1, 0, 0],
+                children: [
+                  {
+                    name: "left-forearm",
+                    translation: [-1, 0, 0],
+                    children: [
+                      {
+                        name: "left-hand",
+                        translation: [-1, 0, 0],
+                        children: [
+                          {
+                            name: "left-hand-plus",
+                            translation: [-1, 0, 0],
+                            children: [
+                              {
+                                name: "left-hand-plus-plus",
+                                translation: [-1, 0, 0],
+                                children: [
+                                  {
+                                    name: "left-hand-plus-plus-plus",
+                                    translation: [-1, 0, 0],
+                                    children: [
+                                      {
+                                        name: "left-hand-plus-plus-plus-plus",
+                                        translation: [-1, 0, 0],
+                                      },
+                                    ],
+                                  },
+                                ],
+                              },
+                            ],
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                ],
+              },
+              {
+                name: "right-arm",
+                translation: [1, 0, 0],
+                children: [
+                  {
+                    name: "right-forearm",
+                    translation: [1, 0, 0],
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            name: "left-leg",
+            translation: [-1, -1, 0],
+            children: [
+              {
+                name: "left-calf",
+                translation: [0, -1, 0],
+                children: [
+                  {
+                    name: "left-foot",
+                    translation: [0, 2, 0],
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            name: "right-leg",
+            translation: [1, -1, 0],
+            children: [
+              {
+                name: "right-calf",
+                translation: [0, -1, 0],
+                children: [
+                  {
+                    name: "right-foot",
+                    translation: [0, 2, 0],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+
+  function makeNode(nodeDescription) {
+    var trs = new TRS();
+    var node = new Node(trs);
+    nodeInfosByName[nodeDescription.name] = {
+      trs: trs,
+      node: node,
+    };
+    trs.translation = nodeDescription.translation || trs.translation;
+    if (nodeDescription.draw !== false) {
+      node.drawInfo = {
+        uniforms: {
+          u_colorOffset: [0, 0, 0.6, 0],
+          u_colorMult: [0.4, 0.4, 0.4, 1],
+        },
+        programInfo: programInfo,
+        bufferInfo: cubeBufferInfo,
+        vertexArray: cubeVAO,
+      };
+      objectsToDraw.push(node.drawInfo);
+      objects.push(node);
+    }
+    makeNodes(nodeDescription.children).forEach(function (child) {
+      child.setParent(node);
+    });
+    return node;
+  }
+
+  function makeNodes(nodeDescriptions) {
+    return nodeDescriptions ? nodeDescriptions.map(makeNode) : [];
+  }
+
+  var scene = makeNode(blockGuyNodeDescriptions);
+
+  requestAnimationFrame(drawScene);
+
+  // Draw the scene.
+  function drawScene(time) {
+    time *= 0.001;
+
     twgl.resizeCanvasToDisplaySize(gl.canvas);
 
+    // Tell WebGL how to convert from clip space to pixels
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-    gl.enable(gl.DEPTH_TEST);
-    gl.enable(gl.CULL_FACE);
 
+    gl.enable(gl.CULL_FACE);
+    gl.enable(gl.DEPTH_TEST);
+
+    // Compute the projection matrix
     var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-    var projectionMatrix = m4.perspective(fieldOfViewRadians, aspect, 1, 2000);
+    var projectionMatrix = m4.perspective(fieldOfViewRadians, aspect, 1, 200);
 
     // Compute the camera's matrix using look at.
-    var cameraPosition = [0, 0, 100];
-    var target = [0, 0, 0];
+    var cameraPosition = [4, 3.5, 10];
+    var target = [0, 3.5, 0];
     var up = [0, 1, 0];
     var cameraMatrix = m4.lookAt(cameraPosition, target, up);
 
@@ -52,27 +340,66 @@ function main() {
 
     var viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
 
-    gl.useProgram(meshProgramInfo.program);
+    var adjust;
+    var speed = 3;
+    var c = time * speed;
+    adjust = Math.abs(Math.sin(c));
+    nodeInfosByName["point between feet"].trs.translation[1] = adjust;
+    adjust = Math.sin(c);
+    nodeInfosByName["left-leg"].trs.rotation[0] = adjust;
+    nodeInfosByName["right-leg"].trs.rotation[0] = -adjust;
+    adjust = Math.sin(c + 0.1) * 0.4;
+    nodeInfosByName["left-calf"].trs.rotation[0] = -adjust;
+    nodeInfosByName["right-calf"].trs.rotation[0] = adjust;
+    adjust = Math.sin(c + 0.1) * 0.4;
+    nodeInfosByName["left-foot"].trs.rotation[0] = -adjust;
+    nodeInfosByName["right-foot"].trs.rotation[0] = adjust;
 
-    // ------ Draw the cube --------
+    adjust = Math.sin(c) * 0.4;
+    nodeInfosByName["left-arm"].trs.rotation[2] = adjust;
+    nodeInfosByName["right-arm"].trs.rotation[2] = adjust;
+    adjust = Math.sin(c + 0.1) * 0.4;
+    nodeInfosByName["left-forearm"].trs.rotation[2] = adjust;
+    nodeInfosByName["right-forearm"].trs.rotation[2] = adjust;
+    adjust = Math.sin(c - 0.1) * 0.4;
+    nodeInfosByName["left-hand"].trs.rotation[2] = adjust;
+    nodeInfosByName["left-hand-plus"].trs.rotation[2] = adjust;
+    nodeInfosByName["left-hand-plus-plus"].trs.rotation[2] = adjust;
+    nodeInfosByName["left-hand-plus-plus-plus"].trs.rotation[2] = adjust;
+    nodeInfosByName["left-hand-plus-plus-plus-plus"].trs.rotation[2] = adjust;
 
-    // Setup all the needed attributes.
-    gl.bindVertexArray(cubeVAO);
+    adjust = Math.sin(c) * 0.4;
+    nodeInfosByName["waist"].trs.rotation[1] = adjust;
+    adjust = Math.sin(c) * 0.4;
+    nodeInfosByName["torso"].trs.rotation[1] = adjust;
+    adjust = Math.sin(c + 0.25) * 0.4;
+    nodeInfosByName["neck"].trs.rotation[1] = adjust;
+    adjust = Math.sin(c + 0.5) * 0.4;
+    nodeInfosByName["head"].trs.rotation[1] = adjust;
+    nodeInfosByName["hat1"].trs.rotation[1] = adjust;
+    nodeInfosByName["hat1"].trs.scale = [1.66, 0.15, 1.66];
+    nodeInfosByName["hat2"].trs.scale = [0.7, 0.5, 0.7];
+    nodeInfosByName["hat2"].trs.translation = [0, 1.25, 0];
+    adjust = Math.cos(c * 2) * 0.4;
+    nodeInfosByName["head"].trs.rotation[0] = adjust;
 
-    cubeUniforms.u_matrix = computeMatrix(
-      viewProjectionMatrix,
-      [config.x, config.y, 0],
-      config.rotate
-    );
+    // Update all world matrices in the scene graph
+    scene.updateWorldMatrix();
 
-    // Set the uniforms we just computed
-    twgl.setUniforms(meshProgramInfo, cubeUniforms);
+    // Compute all the matrices for rendering
+    objects.forEach(function (object) {
+      object.drawInfo.uniforms.u_matrix = m4.multiply(
+        viewProjectionMatrix,
+        object.worldMatrix
+      );
+    });
 
-    twgl.drawBufferInfo(gl, cubeBufferInfo);
-    requestAnimationFrame(render);
+    // ------ Draw the objects --------
+
+    twgl.drawObjectList(gl, objectsToDraw);
+
+    requestAnimationFrame(drawScene);
   }
-
-  requestAnimationFrame(render);
 }
 
 main();
